f28cb577b8198ec49f16f3535e2ddd54518bf7e7
Petr Stipek <pipaslot@seznam.cz>
2019-11-19 19:00:09 +0100

    abstract identity provider

--- Pipaslot.Authorization.Web/IdentityProvider.cs
+++ Pipaslot.Authorization.Web/IdentityProvider.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Security.Principal;
-using System.Text;
 using Microsoft.AspNetCore.Http;
 
 namespace Pipaslot.Authorization.Web
 {
-    public class IdentityProvider<TUserId> : IIdentityProvider<TUserId>
+    public class IdentityProvider<TUserId> : IdentityProviderBase<TUserId>
     {
         private readonly IHttpContextAccessor _contextAccessor;
 
@@ -16,74 +12,15 @@ namespace Pipaslot.Authorization.Web
         {
             _contextAccessor = contextAccessor;
         }
-
-        public TUserId GetUserId()
+        
+        protected override ClaimsPrincipal GetClaimPrincipal()
         {
-            var name = _contextAccessor.HttpContext.User.Identity?.Name;
-            if (_contextAccessor.HttpContext.User.Identity is WindowsIdentity)
-            {
-                return WindowsUserNameToId(name);
-            }
-            if (name is TUserId targetValue)
-            {
-                return targetValue;
-            }
-            if (typeof(TUserId) == typeof(int))
-            {
-                return ParseIntFromString(name, "Role ID");
-            }
-            if (typeof(TUserId) == typeof(long))
-            {
-                return ParseLongFromString(name, "Role ID");
-            }
-            throw new NotSupportedException($"Generic attribute TKey of type {typeof(TUserId)} is not supported. Only int, long and string can be used");
+            return _contextAccessor.HttpContext.User;
         }
 
-        protected virtual TUserId WindowsUserNameToId(string name)
+        protected override TUserId WindowsUserNameToId(string name)
         {
             throw new NotImplementedException($"Username conversion for Windows identity is not implemented. Override method {nameof(WindowsUserNameToId)} to add windows identity conversion support");
-        }
-
-        protected TUserId ParseIntFromString(string value, string field)
-        {
-            if (string.IsNullOrWhiteSpace(value)) return default;
-            if (int.TryParse(value, out var result))
-            {
-                return (TUserId)(object)result;
-            }
-            throw new ArgumentOutOfRangeException($"{field}: Expected integer value but got '{value}'");
-        }
-
-        protected TUserId ParseLongFromString(string value, string field)
-        {
-            if (string.IsNullOrWhiteSpace(value)) return default;
-            if (long.TryParse(value, out var result))
-            {
-                return (TUserId)(object)result;
-            }
-            throw new ArgumentOutOfRangeException($"{field}: Expected long value but got '{value}'");
-        }
-
-        public bool IsAuthenticated => _contextAccessor.HttpContext.User.Identity.IsAuthenticated;
-
-        public List<string> GetRoles()
-        {
-            var user = _contextAccessor.HttpContext.User;
-            var roles = user
-                .FindAll(ClaimTypes.Role)
-                .Select(claim => claim.Value)
-                .ToList();
-
-            if (user.Identity is WindowsIdentity win)
-            {
-                var groups = win.Groups?.ToArray() ?? new IdentityReference[0];
-                var windowsRoles = groups
-                    .Select(id => id.Translate(typeof(NTAccount)).Value)
-                    .Where(group => group.Contains("\\"))
-                    .ToArray();
-                roles.AddRange(windowsRoles);
-            }
-            return roles;
         }
     }
 }
---
+++ Pipaslot.Authorization/IdentityProviderBase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace Pipaslot.Authorization
+{
+    public abstract class IdentityProviderBase<TUserId> : IIdentityProvider<TUserId>
+    {
+        protected abstract ClaimsPrincipal GetClaimPrincipal();
+
+        protected abstract TUserId WindowsUserNameToId(string name);
+
+        public TUserId GetUserId()
+        {
+            var user = GetClaimPrincipal();
+            var name = user.Identity?.Name;
+            if (user.Identity is WindowsIdentity)
+            {
+                return WindowsUserNameToId(name);
+            }
+            if (name is TUserId targetValue)
+            {
+                return targetValue;
+            }
+            if (typeof(TUserId) == typeof(int))
+            {
+                return ParseIntFromString(name, "Role ID");
+            }
+            if (typeof(TUserId) == typeof(long))
+            {
+                return ParseLongFromString(name, "Role ID");
+            }
+            throw new NotSupportedException($"Generic attribute TKey of type {typeof(TUserId)} is not supported. Only int, long and string can be used");
+        }
+
+        protected TUserId ParseIntFromString(string value, string field)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return default;
+            if (int.TryParse(value, out var result))
+            {
+                return (TUserId)(object)result;
+            }
+            throw new ArgumentOutOfRangeException($"{field}: Expected integer value but got '{value}'");
+        }
+
+        protected TUserId ParseLongFromString(string value, string field)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return default;
+            if (long.TryParse(value, out var result))
+            {
+                return (TUserId)(object)result;
+            }
+            throw new ArgumentOutOfRangeException($"{field}: Expected long value but got '{value}'");
+        }
+
+        public bool IsAuthenticated => GetClaimPrincipal().Identity.IsAuthenticated;
+
+        public List<string> GetRoles()
+        {
+            var user = GetClaimPrincipal();
+            var roles = user
+                .FindAll(ClaimTypes.Role)
+                .Select(claim => claim.Value)
+                .ToList();
+
+            if (user.Identity is WindowsIdentity win)
+            {
+                var groups = win.Groups?.ToArray() ?? new IdentityReference[0];
+                var windowsRoles = groups
+                    .Select(id => id.Translate(typeof(NTAccount)).Value)
+                    .Where(group => group.Contains("\\"))
+                    .ToArray();
+                roles.AddRange(windowsRoles);
+            }
+            return roles;
+        }
+    }
+}
