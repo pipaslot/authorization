79fc66d7a6222b5a9ce70a30895864aba797dcbe
Petr Stipek <pipaslot@seznam.cz>
2019-11-19 14:22:56 +0100

    update documentation

--- Pipaslot.AuthorizationUI/AuthorizationUIExtensions.cs
+++ Pipaslot.AuthorizationUI/AuthorizationUIExtensions.cs
@@ -3,11 +3,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Pipaslot.Authorization;
 using Pipaslot.Authorization.Web;
+// ReSharper disable InconsistentNaming
 
 namespace Pipaslot.AuthorizationUI
 {
     public static class AuthorizationUIExtensions
     {
+        /// <summary>
+        /// Register middleware exposing permission management for existing roles
+        /// </summary>
         public static IApplicationBuilder UseAuthorizationUI(this IApplicationBuilder builder, Action<AuthorizationUIOptions> setup = null)
         {
             var options = new AuthorizationUIOptions();
@@ -15,6 +19,14 @@ namespace Pipaslot.AuthorizationUI
             return builder.UseMiddleware<AuthorizationUIMiddleware>(options);
         }
 
+        /// <summary>
+        /// Configure serviced for Authorization middleware
+        /// </summary>
+        /// <typeparam name="TUserId">User primary key type</typeparam>
+        /// <typeparam name="TPermissionStore"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="resourceUniqueId">Unique resource ID for permissions belonging to authorization UI</param>
+        /// <returns></returns>
         public static IServiceCollection AddAuthorizationUI<TUserId, TPermissionStore>(this IServiceCollection services, int resourceUniqueId)
             where TPermissionStore : class, IPermissionStore
         {
